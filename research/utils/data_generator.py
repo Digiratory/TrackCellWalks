bac4ef84794b078d0e35dfd26e0834ce84dd6892
Gleb Solyanik <46048691+glebsolyanik@users.noreply.github.com>
2024-10-04 00:44:33 +0300

    feature(TrackCellWalks) add data_generator.py
    data_generator.py responsible for generating frames from video and image

--- research/utils/data_generator.py
+++ research/utils/data_generator.py
@@ -1,127 +1,94 @@
 import cv2
 from scipy.ndimage import gaussian_filter
-from glob import glob
-import os
-import slideio
+from typing import Generator
 import numpy as np
+import os
 
+class ReaderConfig:
+    def __init__(self, frame_count:int=100, start_frame:int=0 , step:int=1, blur_sigma:float=None):
+        self.frame_count = frame_count
+        self.start_frame = start_frame
+        self.step = step
+        self.blur_sigma = blur_sigma
 
-def bacterial_ds_generator(input_dir: str, 
-                           cache_dir: str, 
-                           output_dir: str, 
-                           cache_suffix: str = ""):
-    """
-    Генератор для создания путей к видеофайлам, файлам кэша и анимациям.
+class VideoReader(ReaderConfig):
+    def __init__(self, video_path: str) -> None:
+        super().__init__()
+        self.video_path: str = video_path
+        self.cap: cv2.VideoCapture = None
 
-    Args:
-        input_dir (str): Путь к папке с видеофайлами.
-        cache_dir (str): Путь к папке, где будут храниться файлы кэша.
-        output_dir (str): Путь к папке, где будут храниться файлы анимаций.
-        cache_suffix (str, optional): Суффикс для файлов кэша. Defaults to "".
+    def frames_generator(self):
+        """Функция возвращает генератор кадров видео."""
 
-    Returns:
-        tuple: Кортеж из двух строк, представляющих путь к видеофайлу и путь к файлу кэша.
-    """
+        self._open_video()
 
-    videos = glob(os.path.join(input_dir, f"*.avi"))
-    for input_file in videos:
-        path_file, _ = os.path.splitext(input_file)
-        file_name = os.path.basename(path_file) 
-        cache_file = os.path.join(cache_dir, file_name + cache_suffix + ".npz")
+        self._setStartFrameInVideo(self.start_frame)
         
-        if cache_suffix == "":
-            output_file = os.path.join(output_dir, file_name + ".mp4")
-            fluctuation_file = os.path.join(output_dir, file_name + ".csv")
-            yield input_file, cache_file, output_file, fluctuation_file
-        else:
-            yield input_file, cache_file
-
-def zvi_based_frame_generator(video_path: str, 
-                              start_frame: int = 0, 
-                              step: int = 1, 
-                              blur_sigma: float = None, 
-                              frame_count: int = np.iinfo(int).max):
-    """
-    Генерирует кадры изображений из файла формата ZVI.
-
-    Args:
-        video_path (str): Путь к файлу ZVI.
-        start_frame (int, optional): Номер первого кадра для чтения. Defaults to 0.
-        step (int, optional): Шаг между кадрами для чтения. Defaults to 1.
-        blur_sigma (float, optional): Стандартное отклонение для размытия изображения (если требуется). Defaults to None.
-        frame_count (int, optional): Максимальное количество кадров для чтения. Defaults to максимальное значение для int.
-
-    Yields:
-        generator: Генератор кадров изображений из файла ZVI.
-    """
-
-    slide = slideio.open_slide(video_path,"ZVI")
-    scene = slide.get_scene(0)
-    for frame_i in range(0, frame_count):
-        i = frame_i % scene.num_t_frames
-        yield scene.read_block(rect=(0,0,0,0), size=(0,0), channel_indices=[0], slices=(0,1), frames=(i,i+1))
-
-
-def video_based_frame_generator(video_path: str, 
-                                start_frame: int = 0, 
-                                step: int = 1, 
-                                blur_sigma: float = None, 
-                                frame_count: int = np.iinfo(int).max):
-    """
-    Генерирует кадры видео из файлового источника.
-
-    Args:
-        video_path (str): Путь к видеофайлу.
-        start_frame (int, optional): Номер первого кадра для чтения. Defaults to 0.
-        step (int, optional): Шаг между кадрами для чтения. Defaults to 1.
-        blur_sigma (float, optional): Стандартное отклонение для размытия изображения (если требуется). Defaults to None.
-        frame_count (int, optional): Максимальное количество кадров для чтения. Defaults to максимальное значение для int.
-
-    Yields:
-        generator: Генератор кадров из видео.
-    """
-    cap = cv2.VideoCapture(video_path) 
-    if not cap.isOpened():
-        print("Cannot open camera")
-        return
-    
-    count = start_frame
-    cap.set(cv2.CAP_PROP_POS_FRAMES, count)
-
-    while True:
-        ret, frame = cap.read()
-        if not ret or count>= frame_count:
-            print("Can't receive frame (stream end?). Exiting ...")
-            cap.release()
-            break
-        grey_np = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        if blur_sigma is not None:
-            grey_np = gaussian_filter(grey_np, sigma=blur_sigma)
-        count += step 
+        for next_frame in range(self.start_frame + 1, self.frame_count + 1, self.step):
+            nextFrameExists, frame = self.cap.read()
             
-        cap.set(cv2.CAP_PROP_POS_FRAMES, count)
-        yield grey_np
-    cap.release()
+            if not nextFrameExists:
+                self._close_video()
+                break
 
-def frame_generator(video_path: str, 
-                    start_frame: int = 0, 
-                    step: int = 1, 
-                    blur_sigma: float = None, 
-                    frame_count: int = np.iinfo(int).max):
-    """
-    Определяет тип видеофайла и вызывает соответствующий генератор кадров.
+            gray_frame = self._frameToGrayColor(frame)
 
-    Args:
-        video_path (str): Путь к видеофайлу.
-        start_frame (int, optional): Номер первого кадра для чтения. Defaults to 0.
-        step (int, optional): Шаг между кадрами для чтения. Defaults to 1.
-        blur_sigma (float, optional): Стандартное отклонение для размытия изображения (если требуется). Defaults to None.
-        frame_count (int, optional): Максимальное количество кадров для чтения. Defaults to максимальное значение для int.
+            gray_frame = self._noiseReduction(gray_frame)
 
-    Returns:
-        generator: Генератор кадров из видео.
-    """
-    if video_path.endswith("zvi"):
-        return zvi_based_frame_generator(video_path, start_frame, step, blur_sigma, frame_count)
-    else:
-        return video_based_frame_generator(video_path, start_frame, step, blur_sigma, frame_count)
+            self._setStartFrameInVideo(next_frame)
+
+            yield gray_frame
+        self._close_video()
+
+    def _open_video(self) -> None:    
+        self.cap = cv2.VideoCapture(self.video_path)
+        if not self._isVideoOppend():
+            raise Exception("Open video error!")
+        
+    def _isVideoOppend(self) -> bool:
+        if self.cap.isOpened():
+            return True
+        return False
+        
+    def _setStartFrameInVideo(self, frame) -> None:
+        self.cap.set(cv2.CAP_PROP_POS_FRAMES, frame)
+
+    def _close_video(self) -> None:
+        self.cap.release()
+    
+    def _frameToGrayColor(self, frame):
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)    
+
+    def _noiseReduction(self, grey_frame) -> np.array:
+        if self.blur_sigma is not None:
+            grey_frame = gaussian_filter(grey_frame, sigma=self.blur_sigma)
+        return grey_frame
+
+class FolderWithVideosReader(ReaderConfig):
+    def __init__(self, folder_path:str) -> None:
+        super().__init__()
+        self.folder_path = folder_path
+        self.videos = os.scandir(self.folder_path)
+
+    def frames_generator(self, video):
+        video_reader = VideoReader(video.path)
+        return video_reader.frames_generator()
+        
+class FolderWithImagesReader(ReaderConfig):
+    def __init__(self, folder_path:str) -> None:
+        super().__init__()
+        self.folder_path = folder_path
+        self.images = os.scandir(self.folder_path)
+
+    def frames_generator(self):
+        for i, image in enumerate(self.images):
+            if i + 1 >= self.frame_count:   # i+1, так как enumerate считает с нуля
+                break
+            grey_frame = cv2.imread(image.path, cv2.COLOR_BGR2GRAY)
+            grey_frame = self._noiseReduction(grey_frame)
+            yield grey_frame
+
+    def _noiseReduction(self, grey_frame) -> np.array:
+        if self.blur_sigma is not None:
+            grey_frame = gaussian_filter(grey_frame, sigma=self.blur_sigma)
+        return grey_frame
