b26d6cac494efd0a00234382be677901466743cd
glebsolyanik <glebsolyanik03@gmail.com>
2024-10-30 19:51:37 +0300

    test(optical_flow_algorithm.py) add OpticalFlowAlgorithmTest.py

--- research/test/OpticalFlowAlgorithmTest.py
+++ research/test/OpticalFlowAlgorithmTest.py
@@ -1,6 +1,3 @@
-import sys
-
-sys.path.append('../..')
 from research.utils.input_reader import InputReader
 from research.utils.data_generator import VideoReader
 from research.utils.optical_flow import ComputeOpticalFlow
@@ -11,42 +8,48 @@ import unittest
 
 class OpticalFlowAlgorithmVideoFileTest(unittest.TestCase):
     def setUp(self):
-        self.params_reader = InputReader('research/test/inputs/input_video_file.json')
+        self.input_reader = InputReader('research/test/inputs/input_video_file.json')
 
-        self.input_path = self.params_reader.get_input_path()
-        self.cache_path = self.params_reader.get_cache_path()
-        self.result_path = self.params_reader.get_result_path()
-
-        self.base = self.params_reader.get_base()
-        self.smin = self.params_reader.get_smin()
-        self.record_duration = self.params_reader.get_record_duration()
-        self.smax = self.record_duration/2
+        self.video = self.input_reader.get_video_file_path()
+        self.cache_path = self.input_reader.get_cache_path()
 
     def test_ComputeOpticalFlowByFarneback(self):
 
-        video_reader = VideoReader(self.input_path)
+        video_reader = VideoReader(self.video)
         frame_generator = video_reader.frames_generator()
 
         cof = ComputeOpticalFlow(frame_generator, self.cache_path)
-        vertical_components, horizontal_components = cof.compute_optical_flow_by_Farneback()
+        cof.compute_optical_flow_by_Farneback()
 
-    def test_LoadOpticalFlow(self): 
-        cof = ComputeOpticalFlow(self.input_path, self.cache_path)
-        vertical_components, horizontal_components = cof.load_optical_flow_from_cahce()
+        analyzer = OpticalFlowAnalyzer(self.input_reader, cof)
 
-        analyzer = OpticalFlowAnalyzer(horizontal_components, vertical_components)
+        visualizer = OpticalFlowVisualizer(cof, analyzer)
+        visualizer.plot_optical_flow_magnitude_and_vector_field()
+        visualizer.plot_fluctuation_analysis()
 
-        height = analyzer.get_image_height()
-        width = analyzer.get_image_width()
-        step = analyzer.compute_step_between_vectors()
-        
-        visulaizer = OpticalFlowVisualizer(height, width, step)
+    def test_LoadOpticalFlow(self):
+        cof = ComputeOpticalFlow(self.video, self.cache_path)
+        cof.load_optical_flow_from_cache()
 
-        pixel_offset = analyzer.compute_pixel_offset()
-        selected_horizontal_magnitude = analyzer.select_horizontal_magnitude()
-        selected_vertical_magnitude = analyzer.select_vertical_magnitude()
+        analyzer = OpticalFlowAnalyzer(self.input_reader, cof)
 
-        visulaizer.plot_optical_flow_magnitude_and_vector_field(pixel_offset, selected_horizontal_magnitude, selected_vertical_magnitude)
+        visualizer = OpticalFlowVisualizer(cof, analyzer)
+        visualizer.plot_optical_flow_magnitude_and_vector_field()
+        visualizer.plot_fluctuation_analysis()
+
+    def test_ComputeOpticalFlowByTVL1(self):
+        video_reader = VideoReader(self.video)
+        frame_generator = video_reader.frames_generator()
+
+        cof = ComputeOpticalFlow(frame_generator, self.cache_path)
+        cof.compute_optical_flow_by_tvl1()
+
+        analyzer = OpticalFlowAnalyzer(self.input_reader, cof)
+
+        visualizer = OpticalFlowVisualizer(cof, analyzer)
+        visualizer.plot_optical_flow_magnitude_and_vector_field()
+        visualizer.plot_fluctuation_analysis()
+
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
