e1318baa28e8603054c9cc43db928a5ec4d78853
glebsolyanik <glebsolyanik03@gmail.com>
2024-10-30 19:46:49 +0300

    fix(optical_flow.py) fix calculating pixel offset

--- research/utils/optical_flow.py
+++ research/utils/optical_flow.py
@@ -12,6 +12,9 @@ class ComputeOpticalFlow:
         self.__horizontal_components = []
 
     def compute_optical_flow_by_Farneback(self):
+        vertical_components = []
+        horizontal_components = []
+        
         image_previous = next(self.__generator)
         for frame in self.__generator:
             image_next = frame
@@ -34,12 +37,15 @@ class ComputeOpticalFlow:
 
             image_previous = image_next.copy()
             
-            self.__horizontal_components.append(horizontal_component)
-            self.__vertical_components.append(vertical_component)
+            horizontal_components.append(horizontal_component)
+            vertical_components.append(vertical_component)
+
+        self.__horizontal_components = np.array(horizontal_components)
+        self.__vertical_components = np.array(vertical_components)
 
         self._save_to_cache()
 
-        return np.array(self.__vertical_components), np.array(self.__horizontal_components)
+        return self.__vertical_components, self.__horizontal_components
 
     def _save_to_cache(self):
         np.savez(
@@ -49,6 +55,9 @@ class ComputeOpticalFlow:
         )
 
     def compute_optical_flow_by_tvl1(self):
+        vertical_components = []
+        horizontal_components = []
+
         image_previous = next(self.__generator)
         for frame in self.__generator:
             image_next = frame
@@ -57,54 +66,62 @@ class ComputeOpticalFlow:
 
             image_previous = image_next.copy()
 
-            self.__horizontal_components.append(horizontal_component)
-            self.__vertical_components.append(vertical_component)
+            horizontal_components.append(horizontal_component)
+            vertical_components.append(vertical_component)
+
+        self.__horizontal_components = np.array(horizontal_components)
+        self.__vertical_components = np.array(vertical_components)
 
         self._save_to_cache()
 
-        return np.array(self.__vertical_components), np.array(self.__horizontal_components)
+        return self.__vertical_components, self.__horizontal_components
 
-    def load_optical_flow_from_cahce(self):
+    def load_optical_flow_from_cache(self):
         with np.load(self.__cache_path) as npzfile:
                 self.__vertical_components = npzfile["vertical_components"]
                 self.__horizontal_components = npzfile["horizontal_components"]
 
         return self.__vertical_components, self.__horizontal_components
     
-    def compute_pixel_offset(self): 
-        vertical_magnitude = self.compute_vertical_magnitude()
-        horizontal_mangnitude = self.compute_horizontal_magnitude()
+    def compute_pixel_offset(self):
+        vertical_magnitude = self._compute_vertical_magnitude()
+        horizontal_magnitude = self._compute_horizontal_magnitude()
 
-        pixel_offset = np.sqrt(vertical_magnitude ** 2 + horizontal_mangnitude ** 2)
+        pixel_offset = np.sqrt(vertical_magnitude ** 2 + horizontal_magnitude ** 2)
 
         return pixel_offset
     
-    def compute_vertical_magnitude(self):
+    def _compute_vertical_magnitude(self):
         vertical_magnitude = self._compute_mean_components(self.__vertical_components)
         return vertical_magnitude
     
     def _compute_mean_components(self, components):
-        components_quantiled = np.quantile(components, self.__thresh)
+        thresh = 0.5
+        components_quantiled = np.quantile(components, thresh)
         mean = components.mean(
             axis=0, 
             where=components > components_quantiled
         )
         return mean
     
-    def compute_horizontal_magnitude(self):
+    def _compute_horizontal_magnitude(self):
         horizontal_magnitude = self._compute_mean_components(self.__horizontal_components)
         return horizontal_magnitude
     
-    def select_vertical_magnitude(self):
+    def downsample_vertical_magnitude(self):
         step = self.compute_step_between_vectors()
-        vertical_mangintude = self.compute_vertical_magnitude()
-        selected_vertical_magnitude = vertical_mangintude[::step, ::step]
-        return selected_vertical_magnitude
+        vertical_magnitude = self._compute_vertical_magnitude()
+        
+        downsampled_vertical_magnitude = vertical_magnitude[::step, ::step]
+        return downsampled_vertical_magnitude
 
     def compute_step_between_vectors(self):
         height = self.get_image_height()
         width = self.get_image_width()
-        step = max(height // self.__num_displayed_vectors, width // self.__num_displayed_vectors)
+
+        num_displayed_vectors = 25
+
+        step = max(height // num_displayed_vectors, width // num_displayed_vectors)
         return step
     
     def get_image_height(self):
@@ -115,11 +132,12 @@ class ComputeOpticalFlow:
         width = self.__horizontal_components.shape[2]
         return width
 
-    def select_horizontal_magnitude(self):
+    def downsample_horizontal_magnitude(self):
         step = self.compute_step_between_vectors()
-        horiznotal_magnitude = self.compute_horizontal_magnitude()
-        selected_horizontal_magnitude = horiznotal_magnitude[::step, ::step]
-        return selected_horizontal_magnitude
+        horizontal_magnitude = self._compute_horizontal_magnitude()
+
+        downsampled_horizontal_magnitude = horizontal_magnitude[::step, ::step]
+        return downsampled_horizontal_magnitude
 
     def compute_vector_field(self):
         return self.__vertical_components + 1j * self.__horizontal_components
